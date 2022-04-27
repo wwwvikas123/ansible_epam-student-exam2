8e7f8b63b13b616a23e12bb2df1d723acf3ac796
Вика Лосева <wwwvikas123@mail.ru>
2022-04-27 03:29:41 +0300

    added jenkins

--- tests.sh
+++ tests.sh
@@ -1,3 +1,3 @@
 STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" http://192.168.0.102:10002)
 
-if [ "$STATUSCODE" -ne 200 ]; then exit "$STATUSCODE"; fi
+if [ "$STATUSCODE" -ne 200 ]; then exit 1; fi
